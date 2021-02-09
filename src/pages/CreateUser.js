304e974ae43539322d4f265ef30b00591e536ac6
jnicolon <63628726+jnicolon@users.noreply.github.com>
2021-02-08 22:37:55 -0500

    createUser functionality and desing

--- src/pages/CreateUser.js
+++ src/pages/CreateUser.js
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
 function CreateUser() {
-  return <div>Create User</div>;
+  const [userName, setUserName] = useState("");
+  const [error, seterror] = useState(false);
+
+  const handleChange = (e) => {
+    setUserName(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(userName);
+  };
+
+  return (
+    <div className="addSecret-container">
+      <form onSubmit={handleSubmit} className="addSecret-form">
+        <label> Enter your username.</label>
+        <input
+          className="createUser-input"
+          type="text"
+          onChange={handleChange}
+        ></input>
+        {error && (
+          <>
+            <p className="createUser-error">That user already exists!</p>
+            <p className="createUser-error">
+              User must be at least 3 characters long!
+            </p>
+          </>
+        )}
+
+        <button>Submit</button>
+      </form>
+    </div>
+  );
 }
 
 export default CreateUser;
