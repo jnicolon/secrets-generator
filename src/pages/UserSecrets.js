94b07746c31749e02812702f6cbf7afef2cd470c
jnicolon <63628726+jnicolon@users.noreply.github.com>
2021-02-15 15:40:08 -0500

    user secrets

--- src/pages/UserSecrets.js
+++ src/pages/UserSecrets.js
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PostcardList from "../components/PostcardList";
 import { useSelector } from "react-redux";
+import { Redirect } from "react-router-dom";
 
 function UserSecrets() {
-  return <PostcardList />;
+  const secrets = useSelector((state) => state.secrets.secrets);
+  const userName = useSelector((state) => state.users.currentUser);
+  const [userSecrets, setUserSecrets] = useState([]);
+  const isLoggedIn = useSelector((state) => state.users.isLoggedIn);
+
+  useEffect(() => {
+    let tempUserSecrets = secrets.filter(
+      (secret) => secret.userName === userName
+    );
+    setUserSecrets(tempUserSecrets);
+  }, [secrets, userName]);
+
+  return (
+    <>
+      {!isLoggedIn && <Redirect to="/" />}
+      <PostcardList user={true} secrets={userSecrets} />;
+    </>
+  );
 }
 
 export default UserSecrets;
