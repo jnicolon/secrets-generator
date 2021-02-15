aea845804b57d596275624d3c2c082afc7b486c9
jnicolon <63628726+jnicolon@users.noreply.github.com>
2021-02-15 15:29:36 -0500

    ordered secrets at home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import PostcardList from "../components/PostcardList";
 import { useDispatch, useSelector } from "react-redux";
 import { GetSecrets } from "../redux/actions/secretsActions";
@@ -6,12 +6,19 @@ import { GetSecrets } from "../redux/actions/secretsActions";
 function Home() {
   const dispatch = useDispatch();
   const secrets = useSelector((state) => state.secrets.secrets);
+  const [orderedSecrets, setOrderedSecrets] = useState([]);
 
   useEffect(() => {
     dispatch(GetSecrets());
   }, [dispatch]);
 
-  return <PostcardList secrets={secrets} />;
+  useEffect(() => {
+    let tempSecrets = [...secrets];
+    let tempOrderedSecrets = tempSecrets.reverse();
+    setOrderedSecrets(tempOrderedSecrets);
+  }, [secrets]);
+
+  return <PostcardList secrets={orderedSecrets} />;
 }
 
 export default Home;
